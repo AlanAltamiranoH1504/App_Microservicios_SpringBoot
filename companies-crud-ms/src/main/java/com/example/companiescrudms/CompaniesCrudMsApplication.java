2ab334213ade7d3309ae2bfaecd270d093579108
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-03-31 20:59:20 -0600

    add companies ms

---
+++ companies-crud-ms/src/main/java/com/example/companiescrudms/CompaniesCrudMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.companiescrudms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaniesCrudMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompaniesCrudMsApplication.class, args);
+    }
+
+}
