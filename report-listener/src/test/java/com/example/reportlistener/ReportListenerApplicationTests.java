741fb233a2f3acd1bc8d77c7fbff266122505791
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2026-05-06 20:22:02 -0600

    feat: add a definition for communication with cloud streams.

---
+++ report-listener/src/test/java/com/example/reportlistener/ReportListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reportlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
